10ab226f5317eb07b70895aa03662a111911e22e
s0n025o <scott.nicholes@walmart.com>
2021-01-17 04:15:03 -0600

    Added logic to conditionally place border in only inside ordinances.

--- junk_AncestorDetailView/AncestorDetailView.swift
+++ junk_AncestorDetailView/AncestorDetailView.swift
@@ -16,16 +16,21 @@ struct AncestorDetailView: View {
                 .padding()
             HStack(spacing: 0) {
                 ForEach(Ordinance.allCases, id: \.self) { ordinance in
-                    Text(ordinance.rawValue)
-                        .frame(width: 25)
-                        .background(Rectangle()
-                                        .strokeBorder(Color.blue, lineWidth: 1))
-//                                        .border(Color.blue))
+                    if (ordinance == .confirmation || ordinance == .initiatory || ordinance == .endowment || ordinance == .sealingToSpouse) {
+                        Text(ordinance.rawValue)
+                            .frame(width: 25)
+                            .background(Rectangle()
+                                            .strokeBorder(Color.blue, lineWidth: 1)
+                                            .frame(width: 25, height: 25, alignment: .center))
+                    } else {
+                        Text(ordinance.rawValue)
+                            .frame(width: 25)
+                    }
                 }
             }
                 .background(Capsule()
-                            .strokeBorder(Color.blue, lineWidth: 1)
-                            .frame(width: 150, height: 25, alignment: .center))
+                                .strokeBorder(Color.blue, lineWidth: 1)
+                                .frame(width: 150, height: 25, alignment: .center))
         }
     }
 }
