f6ec461aee56d30448db3ae3d1d4141465bdab47
Scott Nicholes <scott93gps@gmail.com>
2021-02-02 04:05:42 -0600

    Removed center line from capsule end.

--- junk_AncestorDetailView/Shapes/CapsuleEnd.swift
+++ junk_AncestorDetailView/Shapes/CapsuleEnd.swift
@@ -9,21 +9,21 @@ import SwiftUI
 
 struct CapsuleEnd: Shape {
     func path(in rect: CGRect) -> Path {
-        return Path({ path in
-            path.move(to: CGPoint(x: rect.midX, y: rect.midY))
+        Path { path in
             path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2, startAngle: .degrees(270), endAngle: .degrees(90), clockwise: true)
-            path.addLines([CGPoint(x: rect.maxX, y: rect.minY),
-                           CGPoint(x: rect.maxX, y: rect.maxY),
-                           CGPoint(x: rect.midX, y: rect.maxY),
-                           CGPoint(x: rect.midX, y: rect.minY)])
-        })
+            path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.midX, y: rect.maxY))
+        }
     }
 }
 
 struct CapsuleEnd_Previews: PreviewProvider {
     static var previews: some View {
         CapsuleEnd()
+            .fill(Color.blue)
+            .overlay(CapsuleEnd().stroke(Color.black, lineWidth: 2))
             .frame(width: 100, height: 100, alignment: .center)
-            .foregroundColor(.blue)
     }
 }
